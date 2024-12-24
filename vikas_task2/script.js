5bee1efb300e4727d6726678215a2e039121cc1c
VikasPrasad27 <vjp66778899@gmail.com>
2024-12-24 22:04:16 +0530

    Add files via upload

---
+++ vikas_task2/script.js
@@ -0,0 +1,12 @@
+function triggerAnimation(elementId, animationClass) {
+    const element = document.getElementById(elementId);
+  
+   
+    element.classList.remove(animationClass);
+  
+  
+    void element.offsetWidth;
+  
+    element.classList.add(animationClass);
+  }
+  
